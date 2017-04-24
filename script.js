9405a4ca33f8ac6c83730b237aec12135ff72b86
Edmilson <edmilsonrobson@gmail.com>
2017-04-24 12:44:14 -0300

    Much better layout and usability

--- script.js
+++ script.js
@@ -1,24 +1,14 @@
 var squares = document.getElementsByClassName("game-square");
+var gameMessage = document.getElementById("game-message");
+var currentColorText = document.getElementById("current-color-text");
+var correctColorRGB;
+var correctSquareIndex;
 
-var correctColorRGB = generateRandomRGB();
-console.log("Correct RGB: " + correctColorRGB);
-var correctSquareIndex = Math.floor(Math.random()*squares.length)
+var newColorsButton = document.getElementById("new-colors-button");
+newColorsButton.addEventListener("click", resetGame);
 
-for (var i = 0 ; i < squares.length ; i++){
-    if (i == correctSquareIndex){
-        squares[i].style.background = correctColorRGB;
-    } else{
-        squares[i].style.background = generateRandomRGB();
-    }
-
-    squares[i].addEventListener("click", function(){
-        if (this.style.background == correctColorRGB){
-            console.log("You got it!");
-        } else{
-            console.log("Nope!");
-        }
-    });
-}
+implementClickListeners();
+resetGame();
 
 function generateRandomRGB(){
     var rValue = Math.floor(Math.random()*255);
@@ -26,4 +16,43 @@ function generateRandomRGB(){
     var bValue = Math.floor(Math.random()*255);
 
     return "rgb(" + rValue + ", " + gValue + ", " + bValue + ")";
+}
+
+function implementClickListeners(){
+    for (var i = 0 ; i < squares.length ; i++){
+        squares[i].addEventListener("click", function(){
+            if (this.style.background == correctColorRGB){
+                displayVictoryMessage();
+            } else{
+                displayFailMessage();
+                this.style.background = "transparent";
+            }
+        });
+    }
+}
+
+function displayVictoryMessage(){
+    displayMessage("Correct!")
+}
+
+function displayFailMessage(){
+    displayMessage("Nope.")
+}
+
+function displayMessage(msg){
+    gameMessage.textContent = msg;
+}
+
+function resetGame(){
+    correctColorRGB = generateRandomRGB();
+    correctSquareIndex = Math.floor(Math.random()*squares.length)
+    currentColorText.textContent = correctColorRGB;
+
+    for (var i = 0 ; i < squares.length ; i++){
+        if (i == correctSquareIndex){
+            squares[i].style.background = correctColorRGB;
+        } else{
+            squares[i].style.background = generateRandomRGB();
+        }
+    }
 }
